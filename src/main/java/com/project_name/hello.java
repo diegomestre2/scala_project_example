0f8091f40904dee29ee222bbbe7f0fb3f9134239
Diego Gomes Tome <diegogomest@gmail.com>
2020-06-27 14:13:54 +0200

    Hello World

---
+++ src/main/java/com/project_name/hello.java
@@ -0,0 +1,7 @@
+package main.java.com.project_name;
+
+public class hello {
+	public static void main(String[] args) {
+		System.out.println("Hello Java!");
+	}
+}
---
+++ src/main/scala/com/project_name/hello.scala
@@ -0,0 +1,5 @@
+package main.scala.com.project_name
+
+object HelloScala extends App {
+  println("Hello Scala")
+}
